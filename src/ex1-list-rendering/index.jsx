f3e84d9b8f68be3211926153f0f45d6140fb4a11
Landrodie <nilsland31@gmail.com>
2021-03-29 12:48:26 +0200

    Add TD04 Ex1

--- src/ex1-list-rendering/index.jsx
+++ src/ex1-list-rendering/index.jsx
@@ -4,4 +4,19 @@ import React from 'react';
 import movies from './movies.json';
 
 // eslint-disable-next-line import/prefer-default-export
-export const ListRenderingExercise = () => <div>hi</div>;
+export const ListRenderingExercise = () => (
+  <div>
+    {movies.map(({ title, storyline, year }) => (
+      <>
+        <h2 key={title}>
+          {title}
+          ,
+          was released in
+          {year}
+        </h2>
+        <p>{storyline}</p>
+      </>
+    ))}
+  </div>
+
+);
